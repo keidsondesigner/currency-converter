b28d11025ec53b46729df67ba3e495618cb16559
Keidson Roby <keidsondesigner@gmail.com>
2023-10-06 22:20:11 -0400

    feat: :sparkles: add and config format currency brl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
-import { NgModule, isDevMode } from '@angular/core';
+import { LOCALE_ID, NgModule, isDevMode } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -12,6 +13,9 @@ import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { hydrationMetaReducer } from './rehydrate_reducer';
 import { environment } from 'src/environments/environment';
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt);
 
 const metaReducers: MetaReducer[] = [ hydrationMetaReducer ]
 
@@ -31,7 +35,9 @@ const metaReducers: MetaReducer[] = [ hydrationMetaReducer ]
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'pt' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
