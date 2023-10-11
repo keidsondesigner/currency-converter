a5ccd33cd821ead9eadb570d1fa88a221d5c0f73
Keidson Roby <keidsondesigner@gmail.com>
2023-10-10 23:50:43 -0400

    feat: :sparkles: Implement NgRx ConsultCurrencys

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CurrencyDataResponse } from '@interfaces/currency-data.interface';
+import { Observable, Subject, takeUntil } from 'rxjs';
+import { ConsultCurrencysFacade } from 'src/app/core/providers/consult-currencys/consult-currencys.facade';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  listCurrencys$!: Observable<CurrencyDataResponse[]>;
+  isLoading!: boolean;
+  error!: any;
+  destroyed$!: Subject<void>;
 
+  constructor( private _consultCurrencysFacade: ConsultCurrencysFacade ) { }
+
+  ngOnInit(): void {
+    this.setup();
+  }
+
+  setup() {
+    this.destroyed$ = new Subject();
+    this._consultCurrencysFacade.clearState();
+    this._consultCurrencysFacade.getList('CAD-BRL');
+    this._consultCurrencysFacade.getList('ARS-BRL');
+    this._consultCurrencysFacade.getList('GBP-BRL');
+
+    this.listCurrencys$ = this._consultCurrencysFacade.selectList$().pipe(
+      takeUntil(this.destroyed$)
+    )
+  }
+
+  onReloadData(dataCode: string) {
+    this._consultCurrencysFacade.getList(dataCode + '-BRL');
+  }
+
+  ngOnDestroy() {
+    this.destroyed$.next();
+    this.destroyed$.complete();
+  }
 }
